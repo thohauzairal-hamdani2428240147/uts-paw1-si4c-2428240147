8b1b34a4eed41397f43f32b5d89f11761ed8b5a5
thohauzairal-hamdani2428240147 <thohauzairal-hamdani_2428240147@mhs.mdp.ac.id>
2026-04-14 12:00:33 +0700

    Comit 2

---
+++ script.js
@@ -0,0 +1,5 @@
+let nama = document.getElementById("nama");
+let jumlah = document.getElementById("jumlah");
+let daftarBelanja = document.getElementById("daftarBelanja");
+
+console.log(nama.value);
